fd22becb4d8de09dc83ba4d5a87ed2eb469d84f8
Thilina Ranathunga <r.thilina@gmail.com>
2018-07-20 21:18:49 +0530

    Sale Invoice Form interface redesign

--- src/main/webapp/app/entities/sale-invoice/sale-invoice-update.component.ts
+++ src/main/webapp/app/entities/sale-invoice/sale-invoice-update.component.ts
@@ -6,12 +6,15 @@ import * as moment from 'moment';
 import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
 import { JhiAlertService } from 'ng-jhipster';
 
-import { ISaleInvoice } from 'app/shared/model/sale-invoice.model';
+import { ISaleInvoice, SaleInvoice } from 'app/shared/model/sale-invoice.model';
 import { SaleInvoiceService } from './sale-invoice.service';
 import { ICustomer } from 'app/shared/model/customer.model';
 import { CustomerService } from 'app/entities/customer';
 import { ISalesPerson } from 'app/shared/model/sales-person.model';
 import { SalesPersonService } from 'app/entities/sales-person';
+import { ISaleInvoiceItem, SaleInvoiceItem } from 'app/shared/model/sale-invoice-item.model';
+import { PurchaseInvoiceItemService } from 'app/entities/purchase-invoice-item';
+import { IPurchaseInvoiceItem } from 'app/shared/model/purchase-invoice-item.model';
 
 @Component({
     selector: 'jhi-sale-invoice-update',
@@ -19,6 +22,7 @@ import { SalesPersonService } from 'app/entities/sales-person';
 })
 export class SaleInvoiceUpdateComponent implements OnInit {
     private _saleInvoice: ISaleInvoice;
+    private saleInvoiceItem: ISaleInvoiceItem;
     isSaving: boolean;
 
     customers: ICustomer[];
@@ -26,15 +30,19 @@ export class SaleInvoiceUpdateComponent implements OnInit {
     salespeople: ISalesPerson[];
     invoiceDate: string;
 
+    purchaseinvoiceitems: IPurchaseInvoiceItem[];
+
     constructor(
         private jhiAlertService: JhiAlertService,
         private saleInvoiceService: SaleInvoiceService,
         private customerService: CustomerService,
         private salesPersonService: SalesPersonService,
+        private purchaseInvoiceItemService: PurchaseInvoiceItemService,
         private activatedRoute: ActivatedRoute
-    ) {}
+    ) { }
 
     ngOnInit() {
+        this.saleInvoiceItem = new SaleInvoiceItem();
         this.isSaving = false;
         this.activatedRoute.data.subscribe(({ saleInvoice }) => {
             this.saleInvoice = saleInvoice;
@@ -51,19 +59,42 @@ export class SaleInvoiceUpdateComponent implements OnInit {
             },
             (res: HttpErrorResponse) => this.onError(res.message)
         );
+        this.purchaseInvoiceItemService.query().subscribe(
+            (res: HttpResponse<IPurchaseInvoiceItem[]>) => {
+                this.purchaseinvoiceitems = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
     }
 
     previousState() {
         window.history.back();
     }
 
+    addSaleInvoiceItem() {
+        if (this.saleInvoice.saleInvoiceItems === undefined) {
+            this.saleInvoice.saleInvoiceItems = [];
+        }
+        this.saleInvoice.amount = this.saleInvoice.amount === undefined ? 0 : this.saleInvoice.amount;
+        this.saleInvoice.saleInvoiceItems.push(this.saleInvoiceItem);
+        this.saleInvoice.amount += this.saleInvoiceItem.quantity * this.saleInvoiceItem.sellingPrice;
+        this.saleInvoiceItem = new SaleInvoiceItem();
+    }
+
+    deleteSaleInvoiceItem(i: number) {
+        this.saleInvoice.amount -= this.saleInvoice.saleInvoiceItems[i].quantity * this.saleInvoice.saleInvoiceItems[i].sellingPrice;
+        this.saleInvoice.saleInvoiceItems.splice(i, 1);
+    }
+
     save() {
-        this.isSaving = true;
-        this.saleInvoice.invoiceDate = moment(this.invoiceDate, DATE_TIME_FORMAT);
-        if (this.saleInvoice.id !== undefined) {
-            this.subscribeToSaveResponse(this.saleInvoiceService.update(this.saleInvoice));
-        } else {
-            this.subscribeToSaveResponse(this.saleInvoiceService.create(this.saleInvoice));
+        if (this.saleInvoice.saleInvoiceItems.length > 0) {
+            this.isSaving = true;
+            this.saleInvoice.invoiceDate = moment(new Date(), DATE_TIME_FORMAT);//this.invoiceDate        
+            if (this.saleInvoice.id !== undefined) {
+                this.subscribeToSaveResponse(this.saleInvoiceService.update(this.saleInvoice));
+            } else {
+                this.subscribeToSaveResponse(this.saleInvoiceService.create(this.saleInvoice));
+            }
         }
     }
 
@@ -73,7 +104,8 @@ export class SaleInvoiceUpdateComponent implements OnInit {
 
     private onSaveSuccess() {
         this.isSaving = false;
-        this.previousState();
+        this.saleInvoice = new SaleInvoice();
+        // this.previousState();
     }
 
     private onSaveError() {
